3afdbcfa2a127efc9f5fb4e6a0a5f2b7fa72b706
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2025-02-16 23:14:51 +0100

    Add project files.

---
+++ Controllers/AgentsController.cs
@@ -0,0 +1,105 @@
+﻿using EstateFInder.Data;
+using EstateFInder.Models;
+using EstateFInder.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Numerics;
+
+namespace EstateFInder.Controllers
+{//localhost:5000/api/agents
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentsController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public AgentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAgents()
+        {
+
+            var allAgents = dbContext.Agents.ToList();
+
+            return Ok(allAgents);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetAgentById(Guid id) {
+            var agent = dbContext.Agents.Find(id);
+
+            if (agent is null){
+                return NotFound();
+            }
+
+            return Ok(agent);
+        }
+
+        
+
+        [HttpPost]
+        public IActionResult CreateAgent(AddAgentDetail addAgentDetail)
+        {
+            var agentEntity = new Agent()
+            {
+                Name = addAgentDetail.Name,
+                Email = addAgentDetail.Email,
+                Phone = addAgentDetail.Phone,
+                Address = addAgentDetail.Address
+            };
+
+            dbContext.Agents.Add(agentEntity);
+            dbContext.SaveChanges();
+            //return StatusCode(StatusCodes.Status201Created);
+            return Ok(agentEntity);
+        }
+
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateAgentData(Guid id, Update_agent updateAgentData)
+        {
+            var agent = dbContext.Agents.Find(id);
+
+            if (agent is null)
+            {
+                return NotFound();
+            }
+
+
+            agent.Name = updateAgentData.Name;
+            agent.Email = updateAgentData.Email;
+            agent.Phone = updateAgentData.Phone;
+            agent.Address = updateAgentData.Address;
+
+
+            dbContext.SaveChanges();
+            return Ok(agent);
+        }
+
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteAgent(Guid id)
+        {
+            var agent = dbContext.Agents.Find(id);
+
+            if (agent is null)
+            {
+                return NotFound();
+            }
+
+
+            dbContext.Agents.Remove(agent);
+
+
+            dbContext.SaveChanges();
+            return Ok(agent);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+using EstateFInder.Data;
+using EstateFInder.Models.JwtModels;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+         .Build();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AuthDbContext>();
+
+builder.Services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.SaveToken = false;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidIssuer = configuration["JWTSettings:Issuer"],
+            ValidAudience = configuration["JWTSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
+        };
+        o.Events = new JwtBearerEvents()
+        {
+            OnAuthenticationFailed = c =>
+            {
+                c.Response.StatusCode = 401;
+                c.Response.ContentType = "application/json";
+                var result = JsonConvert.SerializeObject("You are not Authorized");
+                return c.Response.WriteAsync(result);
+            },
+            OnForbidden = context =>
+            {
+                context.Response.StatusCode = 403;
+                context.Response.ContentType = "application/json";
+                var result = JsonConvert.SerializeObject("You are not Authorized");
+                return context.Response.WriteAsync(result);
+            },
+        };
+    });
+
+
+
+var app = builder.Build();
+
+
+//app.MapIdentityApi<IdentityUser>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
